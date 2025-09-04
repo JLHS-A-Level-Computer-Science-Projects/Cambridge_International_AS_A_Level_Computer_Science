e8f63f1f6c5194aa9b47a5b68d981560a5e3cb38
4AMAlan <70253940+IGMORINE@users.noreply.github.com>
2025-09-04 16:04:19 +0800

    Add files via upload

---
+++ sortion.py
@@ -0,0 +1,39 @@
+#册那
+import random
+
+list1 = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+def rand():
+	for i in range(0,len(list1)):
+		list1[i] = random.randint(1,100)
+
+def bubblesort():
+    for i in range(len(list1)-1,0,-1):
+        for j in range(0,i):
+            if list1[j] > list1[j + 1]:
+                list1[j],list1[j + 1] = list1[j + 1],list1[j]
+    print(list1)
+
+def improvedbubblesort():
+	NoMoreSwaps = False
+	while not NoMoreSwaps:
+		NoMoreSwaps = True
+		for j in range(0,len(list1)-1):
+			if list1[j] > list1[j + 1]:
+				list1[j],list1[j + 1] = list1[j + 1],list1[j]
+				NoMoreSwaps = False
+	print(list1)
+
+def insertionsort():
+	for i in range(0,len(list1)):
+		ItemToBeInsterted = list1[i]
+		CurrentItem = i - 1
+		while list1[CurrentItem] > ItemToBeInsterted and CurrentItem > -1:
+			list1[CurrentItem + 1] = list1[CurrentItem]
+			CurrentItem = CurrentItem - 1
+		list1[CurrentItem + 1] = ItemToBeInsterted
+	print(list1)
+
+rand()
+print(list1)
+improvedbubblesort()
