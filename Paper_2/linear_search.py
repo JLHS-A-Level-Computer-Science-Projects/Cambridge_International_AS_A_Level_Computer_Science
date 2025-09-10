8a9191013dcce1a800c262a16b9ac27133c5fcfc
4AMAlan <70253940+4AMAlan@users.noreply.github.com>
2025-09-10 21:44:12 +0800

    Add linear search

---
+++ Paper_2/linear_search.py
@@ -0,0 +1,23 @@
+#Created by 4AMAlan
+
+import random
+
+ListToBeSearchedFrom = [0 for i in range(100)] #创建一个拥有100个元素的列表
+ItemToBeSearched = 100
+
+for i in range(100):
+    ListToBeSearchedFrom[i] = random.randint(1,1000) #给列表里所有元素赋随机整数值
+
+print(ListToBeSearchedFrom)
+
+Found = False #标志型变量，找到值为True，没找到值为False
+
+for i in ListToBeSearchedFrom: #遍历列表
+    if i == ItemToBeSearched:
+        Found = True #找到后修改标志型变量的值为True
+        break
+
+if Found == True:
+    print("Found") #找到输出
+else:
+    print("404 Not Found") #没找到输出
