5c9ed2a8d484a04effa2b290d58e989bd94e9faa
xhc2008 <13913016861@163.com>
2026-01-09 22:16:03 +0800

    feat: 功能函数

--- homework/20260109-1.py
+++ homework/20260109-1.py
@@ -34,13 +34,18 @@ class Book(LibraryItem):
     def GetIsRequested(self):
         return self.__IsRequested
 
-    def SetIsRequested(self):
+    def SetIsRequested(self, borrower_id):
         self.__IsRequested = True
+        self.__RequestedBy = borrower_id
+
+    def GetRequestedBy(self):
+        return self.__RequestedBy
 
     def PrintDetails(self):
         print("Book Details")
         LibraryItem.PrintDetails(self)
         print("Is Requested:", self.__IsRequested)
+        print("Requested By:", self.__RequestedBy)
 
 class CD(LibraryItem):
     def __init__(self, t, a, i):
@@ -85,3 +90,147 @@ class Borrower:
         print("Email Address:", self.__EmailAddress)
         print("Borrower ID:", self.__BorrowerID)
         print("Items on Loan:", self.__ItemsOnLoan)
+
+def borrow_book():
+    book_id = int(input("Enter book ID: "))
+    borrower_id = int(input("Enter borrower ID: "))
+    
+    for book in books:
+        if book._LibraryItem__ItemID == book_id:
+            for borrower in borrowers:
+                if borrower.GetBorrowerID() == borrower_id:
+                    book.Borrowing()
+                    borrower.UpdateItemOnLoan(borrower.GetItemsOnLoan() + 1)
+                    print("Book borrowed successfully")
+                    return
+    print("Book or borrower not found")
+
+def return_book():
+    book_id = int(input("Enter book ID: "))
+    borrower_id = int(input("Enter borrower ID: "))
+    
+    for book in books:
+        if book._LibraryItem__ItemID == book_id:
+            for borrower in borrowers:
+                if borrower.GetBorrowerID() == borrower_id:
+                    book.Returning()
+                    borrower.UpdateItemOnLoan(borrower.GetItemsOnLoan() - 1)
+                    print("Book returned successfully")
+                    return
+    print("Book or borrower not found")
+
+def borrow_cd():
+    cd_id = int(input("Enter CD ID: "))
+    borrower_id = int(input("Enter borrower ID: "))
+    
+    for cd in cds:
+        if cd._LibraryItem__ItemID == cd_id:
+            for borrower in borrowers:
+                if borrower.GetBorrowerID() == borrower_id:
+                    cd.Borrowing()
+                    borrower.UpdateItemOnLoan(borrower.GetItemsOnLoan() + 1)
+                    print("CD borrowed successfully")
+                    return
+    print("CD or borrower not found")
+
+def return_cd():
+    cd_id = int(input("Enter CD ID: "))
+    borrower_id = int(input("Enter borrower ID: "))
+    
+    for cd in cds:
+        if cd._LibraryItem__ItemID == cd_id:
+            for borrower in borrowers:
+                if borrower.GetBorrowerID() == borrower_id:
+                    cd.Returning()
+                    borrower.UpdateItemOnLoan(borrower.GetItemsOnLoan() - 1)
+                    print("CD returned successfully")
+                    return
+    print("CD or borrower not found")
+
+def request_book():
+    book_id = int(input("Enter book ID: "))
+    borrower_id = int(input("Enter borrower ID: "))
+    
+    for book in books:
+        if book._LibraryItem__ItemID == book_id:
+            book.SetIsRequested(borrower_id)
+            print("Book requested successfully")
+            return
+    print("Book not found")
+
+def print_all_details():
+    print("\n=== ALL BORROWERS ===")
+    for borrower in borrowers:
+        borrower.PrintDetails()
+        print()
+    
+    print("\n=== ALL BOOKS ===")
+    for book in books:
+        book.PrintDetails()
+        print()
+    
+    print("\n=== ALL CDs ===")
+    for cd in cds:
+        cd.PrintDetails()
+        print()
+
+def add_new_borrower():
+    name = input("Enter borrower's name: ")
+    borrower_id = int(input("Enter borrower's ID: "))
+    borrowers.append(Borrower(name, borrower_id))
+    print("Added")
+
+def add_new_book():
+    title = input("Enter book title: ")
+    author = input("Enter author: ")
+    item_id = int(input("Enter book ID (integer): "))
+    books.append(Book(title, author, item_id))
+    print("Added")
+
+def add_new_cd():
+    title = input("Enter CD title: ")
+    artist = input("Enter artist: ")
+    item_id = int(input("Enter CD ID (integer): "))
+    cds.append(CD(title, artist, item_id))
+    print("Added")
+
+
+borrowers = []
+books = []
+cds = []
+while True:
+    print("1 - Add a new borrower")
+    print("2 - Add a new book")
+    print("3 - Add a new CD")
+    print("4 - Borrow a book")
+    print("5 - Return a book")
+    print("6 - Borrow a CD")
+    print("7 - Return a CD")
+    print("8 - Request book")
+    print("9 - Print all details")
+    print("99 - Exit program")
+
+    choice = int(input("Enter your menu choice: "))
+
+    if choice == 1:
+        add_new_borrower()
+    elif choice == 2:
+        add_new_book()
+    elif choice == 3:
+        add_new_cd()
+    elif choice == 4:
+        borrow_book()
+    elif choice == 5:
+        return_book()
+    elif choice == 6:
+        borrow_cd()
+    elif choice == 7:
+        return_cd()
+    elif choice == 8:
+        request_book()
+    elif choice == 9:
+        print_all_details()
+    elif choice == 99:
+        break
+    else:
+        print("Invalid")
