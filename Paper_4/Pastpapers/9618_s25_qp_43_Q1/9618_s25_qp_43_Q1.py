3f2af3467702e0a54e427fba0728f22fff401e60
4AMAlan <wangshengkai1518@foxmail.com>
2026-04-16 19:55:31 +0800

    feat: New pp

---
+++ Paper_4/Pastpapers/9618_s25_qp_43_Q1/9618_s25_qp_43_Q1.py
@@ -0,0 +1,57 @@
+#Created by 4AMAlan
+
+Queue = [-1 for i in range(50)]
+HeadPointer = -1
+TailPointer = -1
+NumberOfElements = 0
+
+def Enqueue(NewInt) -> bool:
+	global Queue, HeadPointer, TailPointer, NumberOfElements
+	if NumberOfElements == 50:
+		return False	
+	if HeadPointer == -1:
+		Queue[0] = NewInt
+		HeadPointer = 0
+		TailPointer = 0
+		NumberOfElements += 1
+		return True
+	else:
+		Queue[TailPointer + 1] = NewInt
+		TailPointer += 1
+		return True
+
+def Dequeue() -> int:
+	global Queue, HeadPointer, TailPointer, NumberOfElements
+	if NumberOfElements == 0:
+		return -1
+	else:
+		HeadPointer += 1
+		return Queue[HeadPointer - 1]
+
+def CreateQueue():
+	global Queue, HeadPointer, TailPointer, NumberOfElements
+	try:
+		f = open("QueueData.txt", "r")
+	except:
+		print("No such file or directory.")
+	while True:
+		try:
+			Content = f.readline().strip()
+		except:
+			print("Reading Error")
+		if Content == "":
+				break		
+		Enqueue(int(Content))
+		#print(Queue)
+	if NumberOfElements == 50:
+		print("The queue is full.")
+	f.close()
+
+CreateQueue()
+Sum = 0
+while True:
+	Result = Dequeue()
+	if Result == -1:
+		break
+	Sum += Result
+print(Sum)
